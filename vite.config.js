b1611c0901dee9df6de2d472527c1d806fe07f08
150horse-power <150horse-power@rambler.ru>
2025-07-26 23:34:24 +0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [react()],
     server: {
-        port: 64335,
+        port: 3000,
+        open: true
     }
 })
